9a856dcd6a840556d99248de4639376191aca280
sweenutt <sweenutt@192.168.3.19>
2020-10-20 02:18:48 +0700

    Latihan 13 Program Relational

---
+++ src/pbo2_if2_10116414_latihan13_relational/PBO2_IF2_10116414_Latihan13_Relational.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2_if2_10116414_latihan13_relational;
+
+/**
+ *
+ * @author sweenutt
+ * NAMA         : FAEL ANTONI WIJAYA
+ * KELAS        : PBO-2
+ * NIM          : 10116414
+ * Deskripsi    : Program Relational
+ */
+public class PBO2_IF2_10116414_Latihan13_Relational {
+    public static void main(String[] args) {
+           int bil1 = 10;
+           int bil2 = 20;
+           
+           System.out.println("a == b = " + (bil1 == bil2));
+           System.out.println("a != b = " + (bil1 != bil2));
+           System.out.println("a > b = " + (bil1 > bil2));
+           System.out.println("a < b = " + (bil1 < bil2));
+           System.out.println("b >= a = " + (bil2 >= bil1));
+           System.out.println("b <= a =" + (bil2 <= bil1));
+           
+    }
+    
+}
